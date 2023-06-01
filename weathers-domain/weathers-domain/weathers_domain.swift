a658da8f54ba1ef70a715ef0da876fe07dea2688
ez.ios.kwon <ez.ios.kwon@gmail.com>
2023-06-01 12:55:19 +0900

    add project

---
+++ weathers-domain/weathers-domain/weathers_domain.swift
@@ -0,0 +1,10 @@
+//
+//  weathers_domain.swift
+//  weathers-domain
+//
+//  Created by ios.kwon on 2023/06/01.
+//
+
+class weathers_domain {
+
+}
---
+++ weathers_app/weathers_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weathers_app
+//
+//  Created by ios.kwon on 2023/06/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
